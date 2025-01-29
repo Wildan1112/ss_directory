a093a557dbb97569d515ebd447ee8f6384288f46
Wildan Malul Azam <malula0101@gmail.com>
2025-01-29 11:55:48 +0700

    SearchForm componrnts

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,12 @@
 import { SearchForm } from "@/components/SearchForm";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
   return (
     <>
       <section className="pink_container">
@@ -13,7 +18,7 @@ export default function Home() {
           Submit your ideas, we will bring it to life.
         </p>
 
-        <SearchForm />
+        <SearchForm query={query} />
       </section>
     </>
   );
--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -1,20 +1,21 @@
 import Form from "next/form";
 import { SearchFormReset } from "./SearchFormReset";
-export const SearchForm = () => {
-  const query = "Test";
-
+import { Search } from "lucide-react";
+export const SearchForm = ({ query }: { query?: string }) => {
   return (
     <Form action="/" scroll={false} className="search-form">
       <input
         name="query"
         defaultValue={query}
         className="search-input"
-        placeholder="Search idea..."
+        placeholder="Search Idea..."
       />
 
       <div className="flex gap-2">
         {query && <SearchFormReset />}
-        <button className="search-btn text-white">S</button>
+        <button className="search-btn text-white">
+          <Search className="size-5" />
+        </button>
       </div>
     </Form>
   );
