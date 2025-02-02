a88ce784488f678a8fcd377b449fcc016b5df7db
Wildan Malul Azam <malula0101@gmail.com>
2025-02-02 09:22:18 +0700

    card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import Card from "@/components/Card";
 import { SearchForm } from "@/components/SearchForm";
 import Image from "next/image";
 
@@ -7,6 +8,22 @@ export default async function Home({
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
+
+  const posts = [
+    {
+      _id: 1,
+      _createdAt: "Today",
+      views: 89,
+      author: {
+        _id: 1,
+        name: "azam",
+      },
+      description: "This is a test description",
+      image: "https://picsum.photos/200/300",
+      category: "IOT",
+      title: "New gen IOT",
+    },
+  ];
   return (
     <>
       <section className="pink_container">
@@ -19,6 +36,22 @@ export default async function Home({
         </p>
 
         <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Ideas"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts.length > 0 ? (
+            posts.map((post: any, index: number) => (
+              <Card key={post._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No ideas found</p>
+          )}
+        </ul>
       </section>
     </>
   );
